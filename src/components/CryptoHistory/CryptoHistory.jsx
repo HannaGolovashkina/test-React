064e964405960e613b4050783001b629fab8b4b5
Hanna Golovashkina <108117285+HannaGolovashkina@users.noreply.github.com>
2023-01-21 20:42:37 +0200

    test

---
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -0,0 +1,27 @@
+import { BaseTable, THead, Th, Tr, Td } from './CryptoHistory.styled';
+import { format } from 'date-fns';
+
+export const CryptoHistory = ({items}) => {
+  return <BaseTable>
+  <THead>
+    <tr>
+      <Th>№</Th>
+      <Th>PRICE</Th>
+      <Th>AMOUNT</Th>
+      <Th>DATE</Th>
+    </tr>
+  </THead>
+
+  <tbody>
+  {items.map(({id, price, amount, date}, index) => (
+
+  <Tr>
+      <Td>{index+1}</Td>
+      <Td>{price}</Td>
+      <Td>{amount}</Td>
+      <Td>{format(new Date(date), 'Pp', { addSuffix: true })}</Td>
+    </Tr>))}
+  
+  </tbody>
+</BaseTable>;
+};
